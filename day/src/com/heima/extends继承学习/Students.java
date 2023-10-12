4e0cc2c4aad622ff005aff06bae19f797965ee0e
killman122 <wjr2483484885@gmail.com>
2023-10-12 20:58:04 +0800

    修改了继承中的错误使用

--- day/src/com/heima/extends继承学习/Students.java
+++ day/src/com/heima/extends继承学习/Students.java
@@ -1,57 +1,26 @@
 package com.heima.extends继承学习;
 
-public class Students {
-    String name;
-    int age;
-    String sex;
-    String stuId;
-
-    public void eat(){
-        System.out.println("吃饭");
-    }
-    public void study(){
-        System.out.println("学习");
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public String getStuId() {
-        return stuId;
-    }
-
-    public void setStuId(String stuId) {
-        this.stuId = stuId;
-    }
+public class Students extends Person{
+    private String id;
 
     public Students() {
     }
 
-    public Students(String name, int age, String sex, String stuId) {
-        this.name = name;
-        this.age = age;
-        this.sex = sex;
-        this.stuId = stuId;
+    public Students(String name, int age, String id) {
+        super(name, age);
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void study() {
+        System.out.println(getName());
+        System.out.println("学习...");
     }
 }
--- day/src/com/heima/extends继承学习/Teachers.java
+++ day/src/com/heima/extends继承学习/Teachers.java
@@ -1,56 +1,25 @@
 package com.heima.extends继承学习;
 
-public class Teachers {
-    String name;
-    int age;
-    String teachID;
-    String sex;
-    public void eat(){
-
-    }
-    public void teach(){
-
-    }
+public class Teachers extends Person{
+    private String workId;
 
     public Teachers() {
     }
 
-    public Teachers(String name, int age, String teachID, String sex) {
-        this.name = name;
-        this.age = age;
-        this.teachID = teachID;
-        this.sex = sex;
+    public Teachers(String name, int age, String workId) {
+        super(name, age);
+        this.workId = workId;
     }
 
-    public String getName() {
-        return name;
+    public String getWorkId() {
+        return workId;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setWorkId(String workId) {
+        this.workId = workId;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getTeachID() {
-        return teachID;
-    }
-
-    public void setTeachID(String teachID) {
-        this.teachID = teachID;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
+    public void teach() {
+        System.out.println("讲课...");
     }
 }
